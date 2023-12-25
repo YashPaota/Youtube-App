3737d02faca415228abf43ea8deda3c1256361ff
Yash Paota <yashpaota@gmail.com>
2023-12-25 14:37:12 +0530

    Created constants.js to put the hardcoded links and data inside it.

---
+++ src/utils/constants.js
@@ -0,0 +1,23 @@
+export const HAMBURGER_ICON =
+  "https://cdn.icon-icons.com/icons2/1875/PNG/512/hamburgermenu_120234.png";
+
+export const YOUTUBE_ICON =
+  "https://www.logo.wine/a/logo/YouTube/YouTube-Logo.wine.svg";
+
+export const PROFILE_ICON =
+  "https://toppng.com/uploads/preview/circled-user-icon-user-pro-icon-11553397069rpnu1bqqup.png";
+
+export const list = [
+  "All",
+  "Mixes",
+  "Music",
+  "Cricket",
+  "Gaming",
+  "News",
+  "Lofi",
+  "Thrillers",
+  "Live",
+  "Standup",
+  "React",
+  "JavaScript",
+];
