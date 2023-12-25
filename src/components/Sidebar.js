9e1aa816fb30ff4985582a1bf339a2a7a5092645
Yash Paota <yashpaota@gmail.com>
2023-12-25 17:56:05 +0530

    applied map inside Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,26 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import SidebarOption from "./SidebarOption";
+// import { Link } from "react-router-dom";
+import { SIDEBAR_OPTIONS } from "../utils/constants";
 
 const Sidebar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
 
-    const isMenuOpen = useSelector(store => store.app.isMenuOpen)
+  if (!isMenuOpen) return null; //This is called as early return method
+  // we can also do or use ternary operator.
+  return (
+    <div className="p-5 shadow-lg w-2/12 border-r-2 border-gray-200">
+      {SIDEBAR_OPTIONS?.map((items, index) => (
+        <SidebarOption name={items.name} icon={items.icon} key={index} />
+      ))}
+    </div>
+  );
+};
 
-    if(!isMenuOpen) return null;    //This is called as early return method
-    // we can also do or use ternary operator.
-    return (
-        <div className="p-5 shadow-lg w-2/12">
-        
-            <ul>
+export default Sidebar;
+
+{
+  /* <ul>
                 <li className="font-semibold cursor-pointer"><Link to="/">Home</Link></li>
                 <li className="font-semibold">Shorts</li>
                 <li className="font-semibold">Subscriptions</li>
@@ -36,9 +46,5 @@ const Sidebar = () => {
                 <li>Learning</li>
                 <li>Podcasts</li>
                 <li>Fashion</li>
-            </ul>
-        </div>
-    )
+            </ul> */
 }
-
-export default Sidebar;
