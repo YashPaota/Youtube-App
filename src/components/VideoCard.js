95a28fd022f10ba57c25c6f622322d276789bc61
Yash Paota <yashpaota@gmail.com>
2023-12-26 16:44:26 +0530

    Modified css

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -3,8 +3,8 @@ const VideoCard = ({ info }) => {
   const { snippet, statistics } = info;
   const { channelTitle, title, thumbnails } = snippet;
   return (
-    <div className="p-2 mx-5  my-4 w-80 h-80 shadow-lg">
-      <img className="rounded-lg" src={thumbnails.medium.url} alt="thumbnail" />
+    <div className="p-2 mx-5  my-4 w-80 h-80 shadow-md rounded-md hover:shadow-slate-400">
+      <img className="rounded-lg " src={thumbnails.medium.url} alt="thumbnail" />
       <ul>
         <li className="font-bold py-2">{title}</li>
         <li>{channelTitle}</li>
