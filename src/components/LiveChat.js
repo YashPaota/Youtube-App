eaa1fb8449e263f0d12c322ad4388a77f8087fd3
Yash Paota <yashpaota@gmail.com>
2023-12-27 12:19:48 +0530

    Added form to live chat to send personalized messages

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
@@ -8,6 +8,7 @@ import { generateRandomName } from "../utils/constants";
 const LiveChat = () => {
   const dispatch = useDispatch();
   const chatMessage = useSelector((store) => store.chat.messages);
+  const [liveMessage, setLiveMessage] = useState();
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -22,13 +23,35 @@ const LiveChat = () => {
     return () => clearInterval(interval);
   }, []);
   return (
-    <div className="  ">
-      
-      <div className=" p-5 flex flex-col-reverse top-10 ml-2 border border-black  bg-slate-100 rounded-lg w-full h-[500px] overflow-y-scroll">
+    <div className="">
+      <div className=" p-5 flex flex-col-reverse top-10 ml-6  border border-black  bg-slate-100 rounded-lg w-[320px] h-[450px] overflow-y-scroll">
         {chatMessage.map((chat, index) => (
           <ChatMessage name={chat.name} message={chat.message} key={index} />
         ))}
       </div>
+
+      <form
+        className="ml-6 p-2 mt-2 w-[320px] border border-black rounded"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(addMessage({
+            name:"Paota",
+            message: liveMessage,
+          }))
+
+          setLiveMessage("");
+        }}
+      >
+        <input
+          className="w-[220px] px-2 py-1 border border-gray-200"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => setLiveMessage(e.target.value)}
+        />
+        <button className="bg-gray-600 text-white w-16 px-2 mx-2 py-1 rounded-md">
+          Send
+        </button>
+      </form>
     </div>
   );
 };
