2fa957c0f0a4e736339836ce883c1879a462fdc0
Yash Paota <yashpaota@gmail.com>
2023-12-27 12:18:42 +0530

    Addded profile pic

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,13 +1,13 @@
-import { PROFILE_ICON } from "../utils/constants";
+import { PROFILE_ICON, generateProfileImage, generateRandomMessage, generateRandomName } from "../utils/constants";
 
 const Comment = ({data}) => {
     const{name , text} = data;
   return (
     <div className="flex bg-gray-200 shadow-sm rounded-md p-2 m-2">
-        <img className="h-12 w-12 ml-2" src={PROFILE_ICON} alt="PROFILE" />
+        <img className="h-12 w-12 ml-2 rounded-full" src={generateProfileImage()} alt="PROFILE" />
         <div className="ml-4">
-            <p className="font-bold">{name}</p>
-            <p>{text}</p>
+            <p className="font-bold">{generateRandomName()}</p>
+            <p>{generateRandomMessage()}</p>
         </div>
     </div>
   )
